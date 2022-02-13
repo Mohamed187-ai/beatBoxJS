58dc59209d28cb2cdc6e2ea323ec1958eca36411
Mohamed Hassan <57568156+Mohamed187-ai@users.noreply.github.com>
2022-02-13 15:48:45 +0200

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,8 +1,3 @@
-/**
- * Beat class that keeps track of playing the audio
- * HINT: Make sure to pass in the audioSrc as parameter to create a new audio track
- * HINT: Create a play function to play the audio if called 
- */
 class Beat {
     constructor(audioSrc){
         this.audio = new Audio(audioSrc);
@@ -12,12 +7,6 @@ class Beat {
         this.audio.play();
     }
 }
-
-
-
-/**
- * Button class that keeps track of the button color based on a press
- */
 class Button {
     constructor(color, keyCode){
         this.color = color;
@@ -26,31 +15,19 @@ class Button {
         this.setButtonColorInHTML();
         this.createTransitionEndListener();
     }
-
     createTransitionEndListener = () => {
         this.element.addEventListener("transitionend", ()=>{
             this.deselect();
         })
     }
-    /**
-     * Set the button color based on color specified
-     */
     setButtonColorInHTML = () => {
         this.element.style.borderColor = this.color;
     }
-
-    /**
-     * Select function to set the background color and boxShadow
-     */
     select = () => {
         this.element.style.backgroundColor = this.color;
         this.element.style.boxShadow = `0px 0px 17px 0px ${this.color}`;
 
     }
-
-    /**
-     * Deselect function to reset background color and boxShadow
-     */
     deselect = () => {
         this.element.style.backgroundColor = "transparent";
         this.element.style.boxShadow = "none";
